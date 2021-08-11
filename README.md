2ffc6cf9ef09875cafd6cf39a70420f8f0ae4e93
gagan <gsgagan85@gmail.com>
2021-08-11 12:00:12 +0300

    Update README file and add licence

---
+++ README.md
@@ -0,0 +1,19 @@
+# Project Title
+### News 
+# Author
+### Gagan Chhabra
+# Description
+
+### This News web application allows a user to read articles from all broadcasting networks around the world.
+# Technologies 
+
+### Python
+### Python flask framework
+### HTML 
+### CSS 
+### Bootstrap
+# BDD
+###  News sources are just one click away and contain articles with url links to different current events.
+# Set Up and Installation
+
+### Navigate to your directory, while in the terminal run this command: git clone  https://github.com/Gagan989/News.git. Then navigate to cloned project directory and launch it in visual studio code.
---
+++ test/new_test.py
@@ -0,0 +1,28 @@
+import unittest
+from app.models import Source,Article
+
+class SourceTest(unittest.TestCase):
+    '''
+    Test class to test the behavior of the source class.
+    '''
+
+    def setUp(self):
+        '''
+        set up method that will run before every test 
+        '''
+        self.new_source=Source("abc","Abc news","this gives timely news","https://www.aljazeera.com/news/","private",)
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,Source))
+
+
+class ArticleTest(unittest.TestCase):
+    '''
+    Test class to test the behavior of the article class.
+    '''
+    def setUp(self):
+        self.new_article=Article("abc","John","mountain goes to the lake", "HEY" ,"the 2022 endpoint","https://www.standardmedia.co.ke/politics/article/2001389638/inside-railas-talks-with-central-elders","2020-2-2","Done deal")
+
+    def  test_instance(self):
+        self.assertTrue(isinstance(self.new_article,Article))
